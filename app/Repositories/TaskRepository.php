cb0dce38e1325f2259101dd459da43146f797eb8
spondonit3 <spn3@spondontit.com>
2022-08-09 19:47:41 +0600

    2nd commit

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use App\Models\TaskList;
+
+class TaskRepository
+{
+    // 
+}
